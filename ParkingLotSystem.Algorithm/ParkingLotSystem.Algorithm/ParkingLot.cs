ae8bfb056e937b5cfcbe785977463a2adbd12395
Lejun Chen <chenlejun@gmail.com>
2025-02-05 22:20:54 +0100

    Create two sln by mistake, need to modify

---
+++ ParkingLotSystem.Algorithm/ParkingLotSystem.Algorithm/ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotSystem.Algorithm
+{
+    public class ParkingLot
+    {
+
+        public ParkingLot() { }
+
+        public void Park(int spotNumber, VehicleType vehicle)
+        {
+
+        }
+
+        public void Leave(int spotNumber, VehicleType vehicle)
+        {
+
+        }
+    }
+}
---
+++ ParkingLotSystem.Algorithm/ParkingLotSystem.Algorithm/ParkingLotData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotSystem.Algorithm
+{
+    public class ParkingLotData
+    {
+        public Dictionary<int, SpotBase> _parkingLotData;
+        public ParkingLotData(Dictionary<int, SpotBase> parkingLotData) { _parkingLotData = parkingLotData; }
+        
+
+        public void ModifyData(int spotNumber, VehicleType vehicle)
+        {
+            {
+
+                if (_parkingLotData.TryGetValue(spotNumber, out SpotBase spot))
+                { spot.Vehicle = vehicle; }
+                else
+                    throw new ParkingException("No such spot number exists in data base");
+                var emptySpotNumber=_parkingLotData.Where(pair=>pair.Value.Vehicle==VehicleType.None).Count();
+                if (emptySpotNumber == 0)
+                {
+                    throw 
+                }
+            }
+        }
+
+        public void ModifyData(int[] spotNumber, VehicleType vehicle)
+        {
+            for (int i = 0; i < spotNumber.Length; i++)
+            {
+                ModifyData(spotNumber[i], vehicle);
+            }
+        }
+    }
+}
